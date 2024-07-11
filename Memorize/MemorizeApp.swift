11acde0df57107d563bc6fc9967e3e6f7817bcb5
Hirbod03 <shoss2@uwo.ca>
2024-07-11 18:58:14 -0400

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Hirbod Hosseini on 2024-07-11.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
